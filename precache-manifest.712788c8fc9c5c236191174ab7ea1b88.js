efac37ac33a3b45bd1fcfded41d1dcf1342f61da
saanuregh <saanu09xs@gmail.com>
2020-03-25 16:17:47 +0530

    Updates

---
+++ precache-manifest.712788c8fc9c5c236191174ab7ea1b88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01f74ee7fb8687bd7f012e73538f73ff",
+    "url": "/covid19-kerala/index.html"
+  },
+  {
+    "revision": "3bd3a16e493cdce0b169",
+    "url": "/covid19-kerala/static/css/main.cc52a8eb.chunk.css"
+  },
+  {
+    "revision": "0e617328af9432a2ef2f",
+    "url": "/covid19-kerala/static/js/2.6eb966cd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid19-kerala/static/js/2.6eb966cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bd3a16e493cdce0b169",
+    "url": "/covid19-kerala/static/js/main.9172c2e3.chunk.js"
+  },
+  {
+    "revision": "2e0d1dcc16d44fdb2746",
+    "url": "/covid19-kerala/static/js/runtime-main.ca7170e1.js"
+  }
+]);
